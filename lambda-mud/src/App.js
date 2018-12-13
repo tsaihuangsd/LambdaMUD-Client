1a3127034cb357ad08aed1a08df03c9dee1726a0
tsaihuangsd <tsaihuangsd@gmail.com>
2018-12-12 16:13:54 -0800

    Day 3 progress:
    Begin implementing front end

--- lambda-mud/src/App.js
+++ lambda-mud/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Route, withRouter} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,23 +7,45 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <nav className="navigation-panel">
+          <h1>Lambda Notes</h1>
+          <br></br>
+          <button onClick={() => this.props.history.push("/")}  
+                  className="notelist-button">
+                  View Your Notes</button><br></br>
+          <button onClick={() => this.props.history.push("/create-note")}  
+                  className="create-note-button">
+                  + Create New Notes</button>
+          </nav>
+          <div className="display-panel">
+          <Route  exact
+                  path='/'
+                  component={NoteListView}/>
+          <Route  path="/note/:id"
+                  component={NoteView}/>
+          <Route  path='/create-note'
+                  component={CreateNoteView}/>
+          <Route  path='/edit/:id'
+                  component={EditView}/>
+          </div>
+
+
       </div>
     );
-  }
+    isAuthenticated() {
+      // Check whether the current time is past the
+      // Access Token's expiry time
+      let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+      return new Date().getTime() < expiresAt;
+    }
+    logout() {
+            // Clear Access Token and ID Token from local storage
+            localStorage.removeItem('access_token');
+            localStorage.removeItem('expires_at');
+            window.location.reload();            
+    }
+  }   
 }
 
-export default App;
+
+export default withRouter(App);
